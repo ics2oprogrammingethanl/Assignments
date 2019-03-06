eb4ff97ea8bb4901edb618de4cca6f1e2e2666c7
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-03-06 18:55:26 -0500

    Added AnimatingImages

--- AnimatingImagesEthanL/main.lua
+++ AnimatingImagesEthanL/main.lua
@@ -4,82 +4,94 @@
 -- Course: ICS2O/3C
 -- This program displays images that animate and move.
 -----------------------------------------------------------------------------------------
+-- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
-
+-- create the background image
 local mushroomBackground = display.newImageRect ( "mushroom.jpg", 1920, 1175 )
 
 mushroomBackground:translate( 512, 384 )
 ------------------------------------------------------------------------------------------
+-- create my local variables and get the physics
 local physics = require( "physics" )
 
 local reginald = display.newImageRect ( "reginald.png", 350, 197 )
-
+-- reginald coordinates/anchor
 reginald.anchorX = 0
 reginald.anchorY = 0
-reginald.x = 150
-reginald.y = 50
+reginald.x = 0
+reginald.y = 0
 
-reginald:translate( 300, 500 )
-reginald.rotation = 1
-
-display.contentWidth = display.contentWidth * 2
-display.contentHeight = display.contentHeight * 2
-
+-- start the physics (all of it)
 physics.start(true)
-local reginald_outline = graphics.newOutline( 2, "reginald.png" )
+
+local reginald_outline = graphics.newOutline( 1, "reginald.png" )
 physics.addBody( reginald, { outline=reginald_outline } )
 
-physics.addBody( reginald, { density=1.0, friction=1, bounce=2 } )
+physics.addBody( reginald, { density=1, friction=1, bounce=1} )
 
 --Create global screen boundaries
-local leftWall = display.newRect(0, 0, 5, 1536 )
+local leftWall = display.newRect(0, 768, 5, 1536)
 local rightWall = display.newRect (1024, 0, 5, 1536)
 local topWall = display.newRect (0, 0, 2048, 5)
-local bottomWall = display.newRect (0, 768, 2048, 5)
-
+local bottomWall = display.newRect (1024, 768, 2048, 5)
+-- add the physics properties to the borders
 physics.addBody(leftWall, "static", { bounce = 1} )
 physics.addBody(rightWall, "static", { bounce = 1} )
 physics.addBody(topWall, "static", { bounce = 1} )
 physics.addBody(bottomWall, "static", { bounce = 1} )
 
+-- make sure the fish doesn't go out the screen
 if reginald.x > display.contentWidth then  reginald.x = display.contentWidth 
 elseif reginald.x < 0 then reginald.x = 0
 end
--- similar thing should be done for Y axis.
-
-scrollSpeed1 = -1
-
+if reginald.y > display.contentWidth then  reginald.y = display.contentWidth 
+elseif reginald.y < 0 then reginald.y = 0
+end
+-- the speed of the fish
+scrollSpeed1 = 1
+-- the way the fish moves in a parabolic path
 local function MoveReginald(event)
 
 	reginald.y = reginald.x + scrollSpeed1
 	reginald.x = reginald.y + scrollSpeed1
 
-	reginald:rotate( 1 )
-
 	reginald:scale(1, 1)
 
+	reginald:rotate( 1 )
 end
-
+-- add the event listener
 Runtime:addEventListener("enterFrame", MoveReginald)
 --------------------------------------------------------------------------------------------
-local myImage3 = display.newImageRect ( "steve.gif", 400, 400 )
+-- create the Steve character
+local steve = display.newImageRect ( "steve.gif", 400, 400 )
 
+-- the speed of Steve
 scrollspeed2 = 1
 
-myImage3:translate( 100, 600 )
-
+steve:translate( 500, 100 )
+-- What moves the steve
 local function MoveSteve(event)
 
-	myImage3.y = myImage3.x + scrollSpeed1
-	myImage3.x = myImage3.y + scrollSpeed1
+	steve:rotate( 3 )
 
-	myImage3:rotate( 3 )
+	steve:scale(1.1 ^ -0.01, 1.1 ^ -0.01)
 
-	myImage3:scale(1, 1)
 end
-
-local myImage4 = display.newImageRect ( "arcticpeeper.png", 250, 150 )
-
-myImage4:translate( 500, 600 )
---------------------------------------------
+-- Add the event listener
+Runtime:addEventListener("enterFrame", MoveSteve)
+-------------------------------------------------------------------------------
+-- create the arctic fish
+local arcticFish = display.newImageRect ( "arcticpeeper.png", 250, 150 )
+-- Set the transparnency of the fish
+arcticFish.alpha = 1
+-- Set the spawn point of the fish
+arcticFish:translate( 500, 600 )
+-- What makes the fish dissapear
+local function dissapearArcticFish(event)
+	arcticFish.alpha = arcticFish.alpha - 0.00000001
+	arcticFish:scale(1.1^0.05, 1.1^0.05)
+end
+-- Add event listener
+Runtime:addEventListener("enterFrame", dissapearArcticFish)
+-------------------------------------------------------------------------
